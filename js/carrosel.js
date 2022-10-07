5a079802069b2365ddeab277dadc7010439af91c
Jackson Luis Fagundes Schirigatti <jacksonschirigatti.fag@gmail.com>
2022-10-06 21:03:46 -0300

    carrosel feito

---
+++ js/carrosel.js
@@ -0,0 +1,61 @@
+let um = document.querySelector('#um');
+let dois = document.querySelector('#dois');
+let tres = document.querySelector('#tres');
+
+let previous = document.querySelector('#previous');
+let next = document.querySelector('#next');
+
+if(window.onload = true) {
+    dois.classList = 'front'
+    tres.classList = 'front'
+}
+
+let cont = 0;
+
+next.addEventListener('click', () => {
+    if(cont == 0) {
+        um.classList = 'back'
+        dois.classList = 'mid'
+        cont++
+        console.log(cont);
+    } else if (cont == 1) {
+        dois.classList = 'back'
+        tres.classList = 'mid'
+        cont++
+
+        console.log(cont);
+
+    } else if (cont == 2) {
+        um.classList = 'mid'
+        dois.classList = 'front'
+        tres.classList = 'front'
+        cont--
+        cont--
+        console.log(cont);
+
+    }
+})
+
+previous.addEventListener('click', () => {
+    if(cont == 2) {
+        tres.classList = 'front'
+        dois.classList = 'mid'
+        cont--
+        console.log(cont);
+
+    } else if (cont == 1) {
+        dois.classList = 'front'
+        um.classList = 'mid'
+        cont--
+        console.log(cont);
+
+    } else if (cont == 0) {
+        um.classList = 'front'
+        dois.classList = 'front'
+        tres.classList = 'mid'
+        cont++
+        cont++
+        console.log(cont);
+
+    }
+})
