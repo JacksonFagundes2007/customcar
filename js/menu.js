691bd0ac74355e284398454b43fc5315a7e6d230
Jackson Luis Fagundes Schirigatti <61524211+JacksonFagundes2007@users.noreply.github.com>
2022-10-08 22:24:57 -0300

    Update carrosel and menu pc and cell

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,37 @@
 let largura = window.screen.width;
 
+let body = document.querySelector('body');
+let menu_pc = document.querySelector('#menu-pc');
+
 if(largura < 900) {
-let body = document.querySelector('#menu-cell').innerHTML = `
-<input type="checkbox" id="active">
-<label for="active" class="menu-btn"><i class="fas fa-bars"></i></label>
-<div class="wrapper">
-  <ul>
-    <li><a href="#">Home</a></li>
-    <li><a href="#">About</a></li>
-    <li><a href="#">Services</a></li>
-    <li><a href="#">Gallery</a></li>
-    <li><a href="#">Feedback</a></li>
-  </ul>
-</div>
-`
+  menu_pc.style.background = "transparent"
+  document.querySelector('.wrapper-fake').classList = 'wrapper';
+
+  body.insertAdjacentHTML('afterbegin', `
+  <input type="checkbox" id="active">
+  <label for="active" class="menu-btn"><i class="fas fa-bars"></i></label>
+  `) 
+  document.querySelector('.wrapper').innerHTML = `
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Gallery</a></li>
+      <li><a href="#">Feedback</a></li>
+    </ul>
+  `
 } else {
-    alert('se a tela for maior que 900px aperarece outro menu aqui')
+  body.insertAdjacentHTML('afterbegin', `
+  <img class="logo-jj" src="images/page/jj-custom-car.png" alt="Logo"></img>
+  `)
+    document.querySelector('#menu-pc').innerHTML = `
+  
+    <ul>
+      <li><a class="link-item" href="index.html">Home</a></li>
+      <li><a class="link-item" href="#models">Models</a></li>
+      <li><a class="link-item" href="index.html">Budget</a></li>
+      <li><a class="link-item" href="index.html">Contact</a></li>
+      <li><a class="link-item" href="index.html">About us</a></li>
+    </ul>
+    `
 }
