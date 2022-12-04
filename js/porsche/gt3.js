1e9818af4de7d5371d65181de9c7bf1076455390
Jackson Luis Fagundes Schirigatti <jacksonschirigatti.fag@gmail.com>
2022-12-03 21:50:36 -0300

    CSS car e GT3 feito - metade panamera

--- js/porsche/gt3.js
+++ js/porsche/gt3.js
@@ -1,5 +1,25 @@
+// cores
+colorsList = ["black","white","red","yellow"]
 
-// color select //
+let menuCores = document.querySelector('#cores-car')
+
+for (let i = 0; i < colorsList.length; i++) {
+    menuCores.insertAdjacentHTML('afterbegin',`
+    <button id="${colorsList[i]}" class="btn-color" id="${colorsList[i]}" ></button>
+    `)
+}
+
+let btns = document.querySelectorAll('.btn-color')
+for (let j = 0; j < btns.length; j++) {
+    btns[j].style = `
+        width: 50px;
+        height: 50px;
+        border: 1px solid rgb(175, 175, 175);
+        cursor: pointer;
+    `
+}
+
+// color select
 let black = document.querySelector('#black');
 let white = document.querySelector('#white');
 let red = document.querySelector('#red');
@@ -7,29 +27,30 @@ let yellow = document.querySelector('#yellow');
 
 
 black.addEventListener('click', ()=>{
-    document.getElementById('imageUm').src = "../images/cars/GT3 RS 2023/black/iris (4).jpg";
-    document.getElementById('imageDois').src = "../images/cars/GT3 RS 2023/black/iris (1).jpg";
-    document.getElementById('imageTres').src = "../images/cars/GT3 RS 2023/black/iris.jpg";
-    document.getElementById('imageQuatro').src = "../images/cars/GT3 RS 2023/black/iris (2).jpg";
+    document.querySelector('#um').src = "../../images/cars/GT3 RS 2023/black/iris (4).jpg";
+    document.querySelector('#dois').src = "../../images/cars/GT3 RS 2023/black/iris (1).jpg";
+    document.querySelector('#tres').src = "../../images/cars/GT3 RS 2023/black/iris.jpg";
+    document.querySelector('#quatro').src = "../../images/cars/GT3 RS 2023/black/iris (2).jpg";
 })
 
 white.addEventListener('click', ()=>{
-        document.getElementById('imageUm').src = "../images/cars/GT3 RS 2023/white/iris.jpg";
-        document.getElementById('imageDois').src = "../images/cars/GT3 RS 2023/white/iris (2).jpg";
-        document.getElementById('imageTres').src = "../images/cars/GT3 RS 2023/white/iris (1).jpg";
-        document.getElementById('imageQuatro').src = "../images/cars/GT3 RS 2023/white/iris (3).jpg";
+        document.querySelector('#um').src = "../../images/cars/GT3 RS 2023/white/iris.jpg";
+        document.querySelector('#dois').src = "../../images/cars/GT3 RS 2023/white/iris (2).jpg";
+        document.querySelector('#tres').src = "../../images/cars/GT3 RS 2023/white/iris (1).jpg";
+        document.querySelector('#quatro').src = "../../images/cars/GT3 RS 2023/white/iris (3).jpg";
 })
 
 red.addEventListener('click', ()=>{
-    document.getElementById('imageUm').src = "../images/cars/GT3 RS 2023/red/iris (3).jpg";
-    document.getElementById('imageDois').src = "../images/cars/GT3 RS 2023/red/iris (1).jpg";
-    document.getElementById('imageTres').src = "../images/cars/GT3 RS 2023/red/iris.jpg";
-    document.getElementById('imageQuatro').src = "../images/cars/GT3 RS 2023/red/iris (2).jpg";
+    document.querySelector('#um').src = "../../images/cars/GT3 RS 2023/red/iris (3).jpg";
+    document.querySelector('#dois').src = "../../images/cars/GT3 RS 2023/red/iris (1).jpg";
+    document.querySelector('#tres').src = "../../images/cars/GT3 RS 2023/red/iris.jpg";
+    document.querySelector('#quatro').src = "../../images/cars/GT3 RS 2023/red/iris (2).jpg";
 })
 
 yellow.addEventListener('click', ()=>{
-    document.getElementById('imageUm').src = "../images/cars/GT3 RS 2023/yellow/iris (4).jpg";
-    document.getElementById('imageDois').src = "../images/cars/GT3 RS 2023/yellow/iris (1).jpg";
-    document.getElementById('imageTres').src = "../images/cars/GT3 RS 2023/yellow/iris.jpg";
-    document.getElementById('imageQuatro').src = "../images/cars/GT3 RS 2023/yellow/iris (2).jpg";
+    document.querySelector('#um').src = "../../images/cars/GT3 RS 2023/yellow/iris (4).jpg";
+    document.querySelector('#dois').src = "../../images/cars/GT3 RS 2023/yellow/iris (1).jpg";
+    document.querySelector('#tres').src = "../../images/cars/GT3 RS 2023/yellow/iris.jpg";
+    document.querySelector('#quatro').src = "../../images/cars/GT3 RS 2023/yellow/iris (2).jpg";
 })
+
--- js/porsche/panamer.js
+++ js/porsche/panamer.js
@@ -1,29 +1,56 @@
+// cores
+colorsList = ["black","white","ametistametalico","papapiametalico"]
+
+let menuCores = document.querySelector('#cores-car')
+
+for (let i = 0; i < colorsList.length; i++) {
+    menuCores.insertAdjacentHTML('afterbegin',`
+    <button id="${colorsList[i]}" class="btn-color" id="${colorsList[i]}" ></button>
+    `)
+}
+
+let btns = document.querySelectorAll('.btn-color')
+for (let j = 0; j < btns.length; j++) {
+    btns[j].style = `
+        width: 50px;
+        height: 50px;
+        border: 1px solid rgb(175, 175, 175);
+        cursor: pointer;
+    `
+}
+
+// color select
 let black = document.querySelector('#black');
 let white = document.querySelector('#white');
-let ametistametalico = document.querySelector('#ametistametalico');
-let papapiametalico = document.querySelector('#papapiametalico');
+let red = document.querySelector('#red');
+let yellow = document.querySelector('#yellow');
 
 
 black.addEventListener('click', ()=>{
-    document.getElementById('imageUm').src = "../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/Black/iris.jpg";
-    document.getElementById('imageDois').src = ".../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/Black/iris (2).jpg";
-    document.getElementById('imageTres').src = "../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/Black/iris (1).jpg";
+    document.querySelector('#um').src = "../../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/black/iris (2).jpg";
+    document.querySelector('#dois').src = "../../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/black/iris (1).jpg";
+    document.querySelector('#tres').src = "../../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/black/iris.jpg";
+    document.querySelector('#quatro').src = "";
 })
 
 white.addEventListener('click', ()=>{
-        document.getElementById('imageUm').src = "../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/white/iris.jpg";
-        document.getElementById('imageDois').src = "../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/white/iris (2).jpg";
-        document.getElementById('imageTres').src = "../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/white/iris (1).jpg";
+        document.querySelector('#um').src = "../../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/white/iris.jpg";
+        document.querySelector('#dois').src = "../../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/white/iris (2).jpg";
+        document.querySelector('#tres').src = "../../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/white/iris (1).jpg";
+        document.querySelector('#quatro').src = "";
 })
 
 ametistametalico.addEventListener('click', ()=>{
-    document.getElementById('imageUm').src = "../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/Ametista_Metalico/iris (2).jpg";
-    document.getElementById('imageDois').src = "../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/Ametista_Metalico/iris (1).jpg";
-    document.getElementById('imageTres').src = "../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/Ametista_Metalico/iris.jpg";
+    document.querySelector('#um').src = "../../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/Ametista_Metalico/iris (2).jpg";
+    document.querySelector('#dois').src = "../../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/Ametista_Metalico/iris (1).jpg";
+    document.querySelector('#tres').src = "../../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/Ametista_Metalico/iris.jpg";
+    document.querySelector('#quatro').src = "";
 })
 
-papapiametalico.addEventListener('click', ()=>{
-    document.getElementById('imageUm').src = "../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/Papapia_Metalico/iris (3).jpg";
-    document.getElementById('imageDois').src = "../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/Papapia_Metalico/iris (1).jpg";
-    document.getElementById('imageTres').src = "../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/Papapia_Metalico/iris.jpg";
+apapiametalico.addEventListener('click', ()=>{
+    document.querySelector('#um').src = "../../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/yellow/iris (2).jpg";
+    document.querySelector('#dois').src = "../../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/yellow/iris (1).jpg";
+    document.querySelector('#tres').src = "../../images/cars/Panamera 4 E-Hybrid Platinum Edition (2023)/yellow/iris.jpg";
+    document.querySelector('#quatro').src = "";
 })
+
