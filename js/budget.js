20e9ae0e16c3c911c87983c108f3aebc614c8117
Jackson Luis Fagundes Schirigatti <jacksonschirigatti.fag@gmail.com>
2022-12-07 10:50:05 -0300

    save jj

--- js/budget.js
+++ js/budget.js
@@ -1,10 +1,19 @@
-let list = ["porsche","aston","audi","Bentley","bmw","Bugatti","buick","cadillac","chevrolet","chrysler","citroen","corvette","dodge","ferrari","fiat","ford","genesis","hyundai","jaguar","jeep","kia","koenigsegg","lamborghini","lexus","maseratti","mazda","mclarem","mercedez","mini","mitsubishi","nissan","pagani","peugeot","renault","rollsroyce","subaru","suzuki","tesla","toyota","volkswagen","volvo","mansory"]
+let list = ["Porsche","Aston","Audi","Bentley","Bmw","Bugatti","Buick","Cadillac","Chevrolet","Chrysler","Citroen","Corvette","Dodge","Ferrari","Fiat","Ford","Genesis","Hyundai","Jaguar","Jeep","Kia","Koenigsegg","Lamborghini","Lexus","Maseratti","Mazda","Mclarem","Mercedez","Mini","Mitsubishi","Nissan","Pagani","Peugeot","Renault","Rollsroyce","Subaru","Suzuki","Tesla","Toyota","Volkswagen","Volvo","Mansory"]
+
+let Porsche = ["Panamera", "Taycan 4S", "GT3 RS", "Macan GTS"]
 
 const select = document.querySelector('#brand-select');
+const model = document.querySelector("#model-select")
+
+let off = document.createAttribute("disable")
 
 for (let i = 0; i < list.length; i++) {
-    console.log("test");
-    
-    select.options[select.options.length] = new Option(`${list[i]}`, `${list[i]}`);
+    select.options[select.options.length] = new Option(`${list[i]}`, `${list[i]}`)
 }
 
+
+    for (let j = 0; j < Porsche.length; j++) {
+        console.log("test");
+        
+        model.options[model.options.length] = new Option(`${Porsche[j]}`, `${Porsche[j]}`)
+    }
